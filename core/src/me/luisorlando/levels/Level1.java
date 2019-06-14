e0a60d51e7e19c296f69ff4a4a43aaebee2e2a0a
Rodrigo Maafs <rodrigoelmaps@gmail.com>
2019-06-14 04:46:50 -0500

    Añadí el inicio del primer nivel.

--- core/src/me/luisorlando/levels/Level1.java
+++ core/src/me/luisorlando/levels/Level1.java
@@ -11,6 +11,8 @@ import me.luisorlando.materials.MaterialType;
 
 public class Level1 implements Level {
 
+    private float floor = 7.35f;
+
     public void play(){
         for (Material m : materiales){
             m.startDraw();
@@ -18,8 +20,20 @@ public class Level1 implements Level {
     }
 
     public void generate(Stage stage, World world){
-        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(5, 15)));
+        //materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(30, 15)));
 
-        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.STICK, stage, world, new Vector2(12, 15)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.STICK, stage, world, new Vector2(43, floor)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.STICK, stage, world, new Vector2(35, floor)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.STICK, stage, world, new Vector2(27, floor)));
+
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(27, floor + 2)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(27, floor + 4)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(27, floor + 6)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(27, floor + 8)));
+
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(43, floor + 2)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(43, floor + 4)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(43, floor + 6)));
+        materiales.add(new MaterialCreator(MaterialType.WOOD, BoxType.BOX, stage, world, new Vector2(43, floor + 8)));
     }
 }
