b09a9ef18c43aa15344fd06e213ed6185d992c8a
Rodrigo Maafs <rodrigo@ticketfactura.com>
2019-06-24 08:48:06 -0500

    Desactivé colisiones con los botones circulares.

--- core/src/me/luisorlando/menus/BotonCirculo.java
+++ core/src/me/luisorlando/menus/BotonCirculo.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Filter;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
@@ -64,6 +65,15 @@ public class BotonCirculo extends Actor implements BotonClickeable {
         fixtureDef.shape = circle;
         body = world.createBody(def);
         fixture = body.createFixture(fixtureDef);
+        disableColission(fixture);
+    }
+
+    private void disableColission(Fixture fixture) {
+        Filter boxBreakFilter = new Filter();
+        boxBreakFilter.categoryBits = 1;
+        boxBreakFilter.groupIndex = 2;
+        boxBreakFilter.maskBits = (short) 0;
+        fixture.setFilterData(boxBreakFilter);
     }
 
     @Override
